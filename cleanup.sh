d149918d9647edc17ee7e96b1e8db89897e74480
EC2 Default User <ec2-user@ip-172-31-54-98.ec2.internal>
2017-02-09 15:22:17 +0000

    latest

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# cleanup.sh - cleanup the CloudFormation stack and the CodeDeploy
+# cleanup.sh - cleanup the CodeDeploy application
 #   application after a successful or failed test.
-#   Cleanup removes all stack and CodeDeploy resources
+#   Cleanup removes all CodeDeploy resources
 echo "Starting cleanup of old resources"
-# Clean up any previous CloudFormation stack 
-aws cloudformation delete-stack --stack-name "cdtutorialStack"
+
 # Clean up any previous CodeDeploy artifacts 
-aws deploy delete-application --application-name cdtutorialMyApp
+aws deploy delete-application --application-name BankScoreCard-Test-Deploy
 echo "Sleeping 3 minutes to allow time for cleanup to complete"
 sleep 180
 echo "End of cleanup script"
--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 # deploy.sh - master script to provision an EC2 instance and
-#   deploy a test application to it using AWS CloudFormation and CodeDeploy
+# deploy a test application to it using AWS CloudFormation and CodeDeploy
+# Configure AWS credentials
 # Cleanup any resources left from previous runs (prevents error msgs.)
+
+./configure-aws.sh
+
 ./cleanup.sh
 # Create a zip archive for myApp with no encompassing directory
-echo "Creating zip archive for myApp"
+echo "Creating zip archive for test app"
 
-zip -qr test.zip *
+sudo zip -qr ./test.zip *
 
 # Upload the archive to S3
-aws s3 cp test.zip s3://deploy-app-storage/test-deploy
+sudo aws s3 cp test.zip s3://deploy-app-storage
 
-#DEBUG aws cloudformation validate-template --template-body $templatebody
-echo "Running CloudFormation to create the stack and instance"
-aws cloudformation create-stack --stack-name "cdtutorialStack" --template-body $templatebody
 # Run CodeDeploy to create a CodeDeploy Application for myApp
 echo "Starting CodeDeploy operations"
-aws deploy create-application --application-name BankScoreCard-Test-Deploy
+sudo aws deploy create-application --application-name BankScoreCard-Test-Deploy
 # Run CodeDeploy to create a CodeDeploy DeploymentGroup for the instances
-aws deploy create-deployment-group --application-name BankScoreCard-Test-Deploy --deployment-group-name BankScoreCard-Test-Deploy --ec2-tag-filters Key=Name,Value=Code-Deploy,Type=KEY_AND_VALUE --service-role-arn arn:aws:iam::262625230303:role/Aws-CodeDeploy
+sudo aws deploy create-deployment-group --application-name BankScoreCard-Test-Deploy --deployment-group-name BankScoreCard-Test-Deploy --ec2-tag-filters Key=Name,Value=Code-Deploy,Type=KEY_AND_VALUE --service-role-arn arn:aws:iam::262625230303:role/Aws-CodeDeploy
 # Run CodeDeploy to create a CodeDeploy Deployment for this revision of myApp
-aws deploy create-deployment --application-name BankScoreCard-Test-Deploy --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name BankScoreCard-Test-Deploy --description "BankScoreCard-Test-Deploy deployment" --s3-location bucket=s3://deploy-app-storage/test-deploy,bundleType=zip,key=test.zip
+sudo aws deploy create-deployment --application-name BankScoreCard-Test-Deploy --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name BankScoreCard-Test-Deploy --description "BankScoreCard-Test-Deploy deployment" --s3-location bucket=deploy-app-storage,bundleType=zip,key=test.zip
 echo "Deployment complete."
 
 
