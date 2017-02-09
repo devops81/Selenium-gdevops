064a192ea3578b5e5c6e6394d6068e04ba9f1582
EC2 Default User <ec2-user@ip-172-31-54-98.ec2.internal>
2017-02-09 18:51:02 +0000

    modified artifact-deploy.sh

--- examples/feed-combiner-java8-webapp/artifact-deploy.sh
+++ examples/feed-combiner-java8-webapp/artifact-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp /home/ec2-user/test/examples/feed-combiner-java8-webapp/target /home/ec2-user/apache-tomcat-8.5.11/webapps 
+cp /home/ec2-user/test/examples/feed-combiner-java8-webapp/target/artifact-deploy.sh /home/ec2-user/apache-tomcat-8.5.11/webapps 
